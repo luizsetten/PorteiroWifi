9522722b7daef2e9bd683032c989326f55261499
Luix Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2019-11-28 11:54:43 -0300

    Funcionando

--- Porteirowifi.ino
+++ Porteirowifi.ino
@@ -5,7 +5,7 @@ const char* ssid = "TP-LINK_3FE4";//Nome da sua rede Wifi
 
 const char* password = "25235173";//Senha da rede
 
-//IP do ESP (para voce acessar pelo browser)
+//IP do ESP (para voce acessar pelo browser)//Reserva de ip feita no roteador
 IPAddress ip(192, 168, 1, 102); //Gateway Padrão. Esse será o valor que vai ser digitado no browser
 
 //IP do roteador da sua rede wifi
@@ -19,12 +19,14 @@ WiFiServer server(80);//Criando o servidor web na porta 80
 
 //Pino do NodeMCU que estara conectado ao rele
 const int pin = 2; //Equivalente ao D4 no NodeMCU
+const int entrada = 0;//Equivalente ao D3 no NodeMCU
 
 //Funcao que sera executada apenas ao ligar o ESP8266
 void setup() {
   Serial.begin(115200);
 //Preparando o pino, que esta lidago ao rele
   pinMode(pin, OUTPUT);
+  pinMode(entrada,INPUT);
 //Isso definirá se a saída começará em HIGH ou LOW quando o sistema for iniciado
   digitalWrite(pin, HIGH);
 //Conectando a rede Wifi
@@ -59,7 +61,7 @@ void loop() {
   buf += "<head><meta charset='UTF-8'><meta http-equiv='cache-control' content='max-age=0' /><meta http-equiv='cache-control' content='no-cache' /><meta http-equiv='expires' content='0' /> <meta http-equiv='expires' content='Tue, 01 Jan 1980 1:00:00 GMT' /><meta http-equiv='pragma' content='no-cache' /><title>PORTEIRO WIFI</title>";
   buf += "<style>body{font-family:Open Sans; color:#555555;} .container .col { float: left; margin: 0 0 1.2em; padding-right: 1.2em; padding-left: 1.2em; } .container { width: 100%; max-width: 1080px;} @media screen and (min-width: 300px) { .container{width: 100%; max-width: 400px; margin: 0 auto;}  }";
   buf += "* {-moz-box-sizing: border-box;} a{text-decoration:none;}  .btn {font-size:   18px;   white-space:nowrap;   width:100%;   padding:.8em   1.5em;   font-family:   Open   Sans,Helvetica,Arial,sans-serif; line-height:18px; display: inline-block;zoom: 1; color: #fff; text-align: center; position:relative;  -webkit-transition: border .25s linear, color .25s linear, background-color .25s linear; transition: border .25s linear, color .25s linear, background-color .25s linear;}";
-  if(digitalRead(pin) == 1){
+  if(digitalRead(entrada) == 1){
     buf += ".btn.btn-sea{background-color:  #32cd32; box-shadow: 0 3px 0 #228b22; border-radius: 100px}";
   } else {
     buf += ".btn.btn-sea{background-color:  #dd1111; box-shadow: 0 3px 0 #8b0000; border-radius: 100px}";
@@ -68,9 +70,13 @@ void loop() {
 
 
 if (req.indexOf("OPCL") != -1){
-    digitalWrite(pin, !digitalRead(pin));
+    digitalWrite(pin,1);
+    Serial.print("Alto");
     delay(500);
+    Serial.print("Baixo");
+    digitalWrite(pin,0);
    }
+   client.flush();
 //Analisando a requisicao recebida para decidir se liga ou desliga a lampada
   
   //Enviando para o browser a 'pagina' criada.
